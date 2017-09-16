1cca790acd687880cfa1aef67e8e265d4a9325cb
Michał Zochniak <michal@zapu.net>
2017-09-16 23:31:07 +0200

    Slow version of inv completing
    Goes straight to Identify. Should call resolve first to get untrusted
    info whether the invitation is a canditate to be completed.

--- go/systests/team_invite_test.go
+++ go/systests/team_invite_test.go
@@ -348,7 +348,10 @@ func TestClearInvitesOnAdd(t *testing.T) {
 
 	t.Logf("Ann created team %q", team)
 
+	bobBadRooter := "other" + bob.username
+
 	ann.addTeamMember(team, bob.username+"@rooter", keybase1.TeamRole_WRITER)
+	ann.addTeamMember(team, bobBadRooter+"@rooter", keybase1.TeamRole_WRITER)
 
 	bob.proveRooter()
 
@@ -367,4 +370,8 @@ func TestClearInvitesOnAdd(t *testing.T) {
 	hasInv, err := t0.HasActiveInvite(keybase1.TeamInviteName(bob.username), "rooter")
 	require.NoError(t, err)
 	require.False(t, hasInv)
+
+	hasInv, err = t0.HasActiveInvite(keybase1.TeamInviteName(bobBadRooter), "rooter")
+	require.NoError(t, err)
+	require.True(t, hasInv)
 }
